03578e82a9c59ec659d25a6857240155e2f44f63
Vittorio <papavittoriowi@gmail.com>
2025-04-03 10:35:11 +0200

    teste

--- evaluate/buggy.py
+++ evaluate/buggy.py
@@ -1,28 +1,33 @@
-# This script contains multiple functions with intentional errors
+
 
 def add_numbers(a, b):
-    return a + b  # Works correctly
+    return a + b 
 
 def subtract_numbers(a, b):
-    return a - c  # 'c' is undefined
+    return a - c 
 
 def multiply_numbers(a, b):
-    return a * b  # Works correctly
+    return a * b  
 
 def divide_numbers(a, b):
-    return a / 0  # Division by zero error
+    return a / 0  
 
 def concatenate_strings(s1, s2):
-    return s1 + s3  # 's3' is undefined
+    return s1 + s3  
 
 def list_index_error():
     my_list = [1, 2, 3]
-    return my_list[5]  # Index out of range
+    return my_list[5]  
 
-# User input
+
 a = input("Enter a number: ")
 b = input("Enter another number: ")
 print("Addition:", add_numbers(a, b))
 print("Subtraction:", subtract_numbers(a, b))
 print("Multiplication:", multiply_numbers(a, b))
 print("Division:", divide_numbers(a, b))
+
+my_list=[0,12,4,5,7,78,9,]
+
+for t in my_list:
+    my_list.append(t+1)
